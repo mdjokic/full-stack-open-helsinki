f27966a5723f3cf5b5825425b795f19f89c36453
mdjokic <djokic.mihailo@gmail.com>
2020-11-09 16:07:47 +0100

    Exercise 1.6 & 1.7: unicafe step 1 and  step 2 completed

---
+++ part 1/unicafe/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h3>Statistics</h3>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      <div>average {(good - bad) / (good + neutral + bad)}</div>
+      <div>positive {good / (good + neutral + bad) * 100}%</div>
+    </div>
+  )
+}
+
+export default App;
