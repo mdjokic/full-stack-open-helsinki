73eb688e47cb768d72effe7de610e196cd207415
mdjokic <djokic.mihailo@gmail.com>
2020-11-28 16:28:43 +0100

    Exercises 2.4 and 2.5: Course information step9 and separate module completed

--- part 2/courseinfo/src/Course.js
+++ part 2/courseinfo/src/Course.js
@@ -1,40 +1,32 @@
-
 const Course = ({ course }) => {
-    return (
-        <div>
-            <Header title={course.name} />
-            <Content
-                parts={course.parts}
-            />
-            <Total
-                parts={course.parts}
-            />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
 
-const Header = ({ title }) => (
-    <h1>{title}</h1>
-)
+const Header = ({ title }) => <h3>{title}</h3>;
 
 const Content = ({ parts }) => (
-    <div>
-        {parts.map(part =>
-            <Part key={part.id} name={part.name} exercise={part.exercises} />
-        )}
-    </div>
-)
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} name={part.name} exercise={part.exercises} />
+    ))}
+  </div>
+);
 
 const Part = ({ name, exercise }) => (
-    <p>{name} {exercise}</p>
-)
+  <p>
+    {name} {exercise}
+  </p>
+);
 
 const Total = ({ parts }) => {
-    const total = parts.reduce(((acc, part) => acc + part.exercises), 0);
-    console.log(total);
-    return (
-    <p>Number of exercises {total}</p>
-    )
-}
+  const total = parts.reduce((acc, part) => acc + part.exercises, 0);
+  return <h4>Number of exercises {total}</h4>;
+};
 
 export default Course;
