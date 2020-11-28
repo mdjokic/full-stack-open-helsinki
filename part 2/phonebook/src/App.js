2458bbe50a39d2b907c5c56a6cce7105e68913e6
mdjokic <djokic.mihailo@gmail.com>
2020-11-28 17:41:04 +0100

    Excercise 2.9: The Phonebook step4 completed

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -1,8 +1,18 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ]);
+
   const [newPerson, setNewPerson] = useState({ name: '', number: '' });
+  const [predicate, setPredicate] = useState('');
+  const [showAll, setShowAll] = useState(true);
+
+  const personToShow = showAll ? persons : persons.filter((person) => person.name.toLowerCase().includes(predicate));
 
   const handleNameChange = (event) => {
     const newName = event.target.value;
@@ -12,6 +22,16 @@ const App = () => {
   const handleNumberChange = (event) => {
     const newNumber = event.target.value;
     setNewPerson({ ...newPerson, number: newNumber });
+  };
+
+  const handlePredicateChange = (event) => {
+    const newPredicate = event.target.value.toLowerCase();
+    if (newPredicate.length === 0) {
+      setShowAll(true);
+    } else {
+      setShowAll(false);
+    }
+    setPredicate(newPredicate);
   };
 
   const handleSubmit = (event) => {
@@ -28,6 +48,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={predicate} onChange={handlePredicateChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newPerson.name} onChange={handleNameChange} />
@@ -40,7 +64,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {personToShow.map((person) => (
         <div key={person.name}>
           {person.name} {person.number}
         </div>
