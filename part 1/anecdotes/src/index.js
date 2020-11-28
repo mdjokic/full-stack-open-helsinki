7803b1b00eb2b6d68d7d724b41a4e498b5a0aa03
mdjokic <djokic.mihailo@gmail.com>
2020-11-28 12:41:22 +0100

    Exercise 1.14: anecdotes step 3

--- part 1/anecdotes/src/index.js
+++ part 1/anecdotes/src/index.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(new Array(props.anecdotes.length).fill(0));
+
+const Anecdote = ({ title, anecdote, votes }) => (
+  <>
+    <h1>{title}</h1>
+    <div>{anecdote}</div>
+    <div>has {votes} votes</div>
+  </>
+)
+
+const App = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(0);
+  const [maxInx, setMaxInx] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
   const handleClick = () => {
-    let anecdoteIndex = Math.round(Math.random() * (props.anecdotes.length - 1));
+    let anecdoteIndex = Math.round(Math.random() * (anecdotes.length - 1));
     while (anecdoteIndex === selected) {
-      anecdoteIndex = Math.round(Math.random() * (props.anecdotes.length - 1));
+      anecdoteIndex = Math.round(Math.random() * (anecdotes.length - 1));
     }
     setSelected(anecdoteIndex);
   }
@@ -17,19 +27,33 @@ const App = (props) => {
     const votesCopy = [...votes];
     votesCopy[selected] += 1;
     setVotes(votesCopy);
+    updateMax(votesCopy);
+  }
+
+  const updateMax = (votesCopy) => {
+    const updatedIndex = votesCopy.reduce(((max, anecdoteVotes, currentIndex) => votes[max] >= anecdoteVotes ? max : currentIndex), 0);
+    setMaxInx(updatedIndex);
   }
 
   return (
     <div>
-      {props.anecdotes[selected]}
       <div>
-        has {votes[selected]} votes
+        <Anecdote
+          title='Anecdote of the day'
+          anecdote={anecdotes[selected]}
+          votes={votes[selected]} />
       </div>
       <div>
         <button onClick={handleVote}>vote</button>
         <button onClick={handleClick}>next anecdote</button>
       </div>
-    </div>
+      <div>
+        <Anecdote
+          title='Anecdote with most votes'
+          anecdote={anecdotes[maxInx]}
+          votes={votes[maxInx]} />
+      </div>
+    </div >
   )
 }
 
