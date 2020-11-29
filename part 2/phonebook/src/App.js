e3c9398d8e7f3aaa09e414fe42bdee0acaf0b3d7
mdjokic <djokic.mihailo@gmail.com>
2020-11-29 15:06:15 +0100

    Exercise 2.11: The Phoneboook step6 completed

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -1,19 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Persons from './Persons';
 import PersonForm from './PersonForm';
 import Filter from './Filter';
+import Axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newPerson, setNewPerson] = useState({ name: '', number: '' });
   const [predicate, setPredicate] = useState('');
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/persons').then((response) => setPersons(response.data));
+  }, []);
 
   const personToShow = showAll ? persons : persons.filter((person) => person.name.toLowerCase().includes(predicate));
 
