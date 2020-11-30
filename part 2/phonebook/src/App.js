e0ef5233f5e9435029aa0ee51120278c37a2ac6b
mdjokic <djokic.mihailo@gmail.com>
2020-11-30 15:16:50 +0100

    Exercise 2.19 and 2.20: Phonebook step11, step12 completed

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import Persons from './Components/Persons';
 import PersonForm from './Components/PersonForm';
 import Filter from './Components/Filter';
+import Notification from './Components/Notification';
 import PersonService from './Services/Person';
 
 const App = () => {
@@ -12,6 +13,11 @@ const App = () => {
   useEffect(() => {
     PersonService.getAll().then((data) => setPersons(data));
   }, []);
+
+  //Notification messages
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const notificationDuration = 5000;
 
   const personToShow = showAll ? persons : persons.filter((person) => person.name.toLowerCase().includes(predicate));
 
@@ -42,14 +48,34 @@ const App = () => {
       PersonService.save(newPerson).then((data) => {
         setPersons(persons.concat(data));
         setNewPerson({ name: '', number: '' });
+        showNotification('success', `Added ${data.name}`);
       });
     } else {
-      if(window.confirm(`${newPerson.name} is already aded to phonebook, replace old number with a new one?`)){
-        PersonService.update(duplicate[0].id, newPerson).then(data => {
-          setPersons(persons.map(person => person.id !== duplicate[0].id ? person : data));
-          setNewPerson({ name: '', number: '' });
+      if (window.confirm(`${newPerson.name} is already aded to phonebook, replace old number with a new one?`)) {
+        PersonService.update(duplicate[0].id, newPerson).then((data) => {
+          setPersons(persons.map((person) => (person.id !== duplicate[0].id ? person : data)));
+          showNotification('success', `Updated ${data.name}`);
         })
+        .catch((_) => {
+          showNotification('error', `${duplicate[0].name} has been deleted from server`);
+          setPersons(persons.filter((person) => person.id !== duplicate[0].id));
+        });
+        setNewPerson({ name: '', number: '' });
       }
+    }
+  };
+
+  const showNotification = (type, message) => {
+    if (type === 'error') {
+      setErrorMessage(message);
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, notificationDuration);
+    } else {
+      setSuccessMessage(message);
+      setTimeout(() => {
+        setSuccessMessage(null);
+      }, notificationDuration);
     }
   };
 
@@ -58,6 +84,11 @@ const App = () => {
       PersonService.remove(id)
         .then((_) => {
           setPersons(persons.filter((person) => person.id !== id));
+          showNotification('success', `${name} has been successfully removed from server`);
+        })
+        .catch((_) => {
+          showNotification('error', `${name} has already been deleted from server`);
+          setPersons(persons.filter((person) => person.id !== id));
         });
     }
   };
@@ -65,6 +96,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={successMessage} type='success' />
+      <Notification message={errorMessage} type='error' />
       <Filter predicate={predicate} handlePredicateChange={handlePredicateChange} />
       <h2>add a new</h2>
       <PersonForm
@@ -74,9 +107,7 @@ const App = () => {
         handleNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-      <Persons 
-        handleDelete={handleDelete} 
-        personToShow={personToShow} />
+      <Persons handleDelete={handleDelete} personToShow={personToShow} />
     </div>
   );
 };
